13785f7eb655e728456bc679f3b6dd3aef83f288
joedunn-n00b <66018451+joedunn-n00b@users.noreply.github.com>
2021-11-13 12:12:49 -0500

    Refactoring the startjobs.py file to save and check new podcasts saved in the DB vs new ones from the rss feed

--- podcasts/management/commands/startjobs.py
+++ podcasts/management/commands/startjobs.py
@@ -5,21 +5,32 @@ from dateutil.parser import parse
 
 from podcasts.models import Episode
 
+# refactor to check current pc's to saved pc's
+def save_new_episodes(feed):
+    podcast_title = feed.channel.title
+    podcast_image = feed.channel.image['href']
+
+    for item in feed.entries():
+        if not Episode.objects.filter(guid=item.guid).exists()
+            episode = Episode(
+                title = item.title,
+                description = item.description,
+                pub_date = parse(item.published),
+                link = item.link,
+                image = podcast_image,
+                podcast_name = podcast_title,
+                guid = item.guid,
+            )
+            episode.save()
+def fetch_realpython_episodes():
+    _feed = fp.parse('https://realpython.com/podcasts/rpp/feed')
+    save_new_episodes(_feed)
+
+def fetch_talkpython_episodes():
+    _feed = fp.parse("https://talkpython.fm/episodes/rss")
+    save_new_episodes(_feed)
+
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        feed = fp.parse('https://realpython.com/podcasts/rpp/feed')
-        podcast_title = feed.channel.title
-        podcast_image = feed.channel.image['href']
-
-        for item in feed.entries:
-            if not Episode.objects.filter(guid=item.guid).exists():
-                episode = Episode(
-                    title = item.title,
-                    description = item.description,
-                    pub_date = parse(item.published),
-                    link = item.link,
-                    image = podcast_image,
-                    podcast_name = podcast_title,
-                    guid = item.guid,
-                )
-                episode.save()
+        fetch_realpython_episodes()
+        fetch_talkpython_episodes()
